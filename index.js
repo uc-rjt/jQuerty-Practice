4ef3eba677559ed97ec766a29bb9bc5f0ae5f227
uc-rjt <rajat.sachan@ucertify.com>
2023-03-14 15:20:55 +0530

    basic syntax and selectors

---
+++ index.js
@@ -0,0 +1,20 @@
+
+$('a').attr('href', 'https://www.google.com');
+
+// console.log($('img').attr('src'));
+
+// $('h1').text('Bye');
+
+// $('button').html('<em>Hey</em>')
+
+// $('button').text("Don't click me");
+
+$('h1').addClass('big-title');
+
+console.log($('h1').attr('class'));
+
+// console.log($('h1').hasClass('big-title'));
+// console.log($('h1').hasClass('big-title'));
+
+// $('h1').removeClass('big-title');
+// $('h1').css('font-size', '5rem');
